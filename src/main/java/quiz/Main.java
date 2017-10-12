6ea3450c2b4b36a28db4ff89adcabb8e0186af8f
mariliis <mariliis.pihlapuu@gmail.com>
2017-10-12 14:19:34 +0300

    Created HelloServlet.java, server seems to be working

--- src/main/java/quiz/Main.java
+++ src/main/java/quiz/Main.java
@@ -1,8 +1,23 @@
 package quiz;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int PORT = Integer.parseInt(System.getenv("PORT"));
+        Server server = new Server(PORT);
+        try {
+            ServletHandler servletHandler = new ServletHandler();
+            server.setHandler(servletHandler);
+
+            servletHandler.addServletWithMapping(HelloServlet.class, "/*");
+
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            System.out.println("Something went wrong during server start");
+        }
     }
 }
